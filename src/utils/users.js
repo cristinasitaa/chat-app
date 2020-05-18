cbd81b2d4baa349ffb092536b66c14ea759e542f
Cristina Sita <c_csita@groupon.com>
2020-05-18 18:35:13 +0300

    Initial commit

---
+++ src/utils/users.js
@@ -0,0 +1,67 @@
+const users = []
+
+const addUser = ({id, username, room}) => {
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required'
+        }
+    }
+
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    if (existingUser) {
+        return {
+            error: 'Username is in use'
+        }
+    }
+
+    const user = {id, username, room}
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => {
+        return user.id === id
+    })
+
+    if (index !== -1) {
+        return users.splice(index, 1)
+    }
+}
+
+const getUser = (id) => {
+    const user = users.find(user => user.id === id)
+
+    if (!user) {
+        return {
+            error: 'Could not find user with the id provided'
+        }
+    }
+
+    return user
+}
+
+const getUsersInRoom = (room) => {
+    const usersInRoom = users.filter(user => user.room === room)
+
+    if (usersInRoom.length === 0) {
+        return {
+            error: 'No users in room'
+        }
+    }
+
+    return usersInRoom
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
